b5539bd94de1f1f678946949ba94d28857524b17
Davíð Guðni <davidgh@ru.is>
2016-11-15 22:55:39 +0000

    added a little bit of error recovery

--- parser_abstract.py
+++ parser_abstract.py
@@ -7,17 +7,21 @@ class Parser():
         self.lexer = lexer
         self.lookahead = lexer.nextToken()
 
-    def _match(self):
-        self.lookahead = self.lexer.nextToken()
+    def _match(self, tokenCode):
+        if(tokenCode == self.lookahead.tokenCode):
+            self.lookahead = self.lexer.nextToken()
+        else:
+            print('Expected ' + tokenCode.name)
+            exit(1)
 
     def _O(self):
         op = self.lookahead.lexeme
-        self._match()
+        self._match(TokenCode.Addop)
         return op
 
     def _T(self):
         num = self.lookahead.lexeme
-        self._match()
+        self._match(TokenCode.Number)
         return num
 
     def parse(self):
